8c168eb4e741216686290c9a917c799f0be7db54
Tomás Vera <tomas.vera@virtualdreams.io>
2021-06-11 15:08:05 -0300

    Update

---
+++ Content/scripts/Card/card-solicitud-digital.js
@@ -0,0 +1,22 @@
+﻿var url_string = window.location.href;
+var url = new URL(url_string);
+//var c = url.searchParams.get("email");
+var token = url.searchParams.get("token");
+var lead = url.searchParams.get("lead");
+/* if(c!='' && c!=null && c!=undefined)
+    c=atob(c); */
+if (token != '' && token != null && token != undefined)
+    token = atob(token);
+
+//document.onreadystatechange = function () {
+//    showLoader();
+//}
+
+$(document).ready(function () {
+    $Lightning.use("c:pruebita3", () => {
+        $Lightning.createComponent("c:frontofficeSolicitudDigital", { "lead": lead, "backUrl": "https://tfs-sitefinity.virtualdreams.io/tfsm/mis-cotizaciones" }, "lightning", () => { console.log("YA SE CREO EL COMPONENTE"); hideLoader(); });
+    },
+        "https://toyotafinancial--salt001.lightning.force.com", token
+    );                        
+
+});
