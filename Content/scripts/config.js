8808f6b978d2bf05c6560e1781fb8c29f28d8e7d
Tomás Vera <tomas.vera@virtualdreams.io>
2021-06-17 01:57:04 -0300

    Update config.js

--- Content/scripts/config.js
+++ Content/scripts/config.js
@@ -1,4 +1,4 @@
 ﻿window.config = {
-    //urlbase: 'https://tfs-sitefinity.virtualdreams.io:444/api/tfsm',
-    urlbase: 'https://localhost:44386/api/tfsm',
+    urlbase: 'https://tfs-sitefinity.virtualdreams.io:444/api/tfsm',
+    //urlbase: 'https://localhost:44386/api/tfsm',
 }
