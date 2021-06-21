1629cad8bd4367aaf3733498bb7f5f042cfbec6d
Tomás Vera <tomas.vera@virtualdreams.io>
2021-06-21 18:01:16 -0300

    Update card-header-logged.js

--- Content/scripts/Card/card-header-logged.js
+++ Content/scripts/Card/card-header-logged.js
@@ -1,7 +1,7 @@
 let isLogged = sessionStorage.getItem("isLogged");
 
 if (isLogged !== "true" && !window.location.href.includes("Sitefinity/adminapp")) {
-  window.location.replace(window.location.origin + "/tfsm/my-tfsm/login-clientes");
+  //window.location.replace(window.location.origin + "/tfsm/my-tfsm/login-clientes");
 }
 
 $(document).ready(function () {
