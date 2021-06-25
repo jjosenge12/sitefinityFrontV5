c79db91328facb2ee142798e1dfb0c3c54577090
Tomás Vera <tomas.vera@virtualdreams.io>
2021-06-25 11:32:32 -0300

    Update

--- Content/scripts/Cotizador/cotizador.js
+++ Content/scripts/Cotizador/cotizador.js
@@ -51,7 +51,6 @@ $(document).ready(function () {
     swiper = new Swiper('#cotizador-swiper-container', {
         speed: 400,
         allowTouchMove: false,
-        initialSlide: 2,
         autoHeight: true
     });
 
