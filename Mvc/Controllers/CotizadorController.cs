a57ad7ee7fea8b0247ea3a4daf43128bc417c7ab
Tomás Vera <tomas.vera@virtualdreams.io>
2021-07-01 14:09:01 -0300

    Update

--- Mvc/Controllers/CotizadorController.cs
+++ Mvc/Controllers/CotizadorController.cs
@@ -32,8 +32,7 @@ namespace SitefinityWebApp.Mvc.Controllers
 		public ActionResult Index()
 		{
 			var model = new CotizadorModel();
-
-			//model.MontoEnganche = this.MontoEnganche;
+			model.MontoEnganche = this.MontoEnganche;
 			model.autos = GetAllAutos();
 
 			return View("Index", model);
@@ -44,7 +43,7 @@ namespace SitefinityWebApp.Mvc.Controllers
             this.ActionInvoker.InvokeAction(this.ControllerContext, "Index");
         }
 
-		public string Message { get; set; }
+		public int MontoEnganche { get; set; }
 		public string SerializedSelectedItemId { get; set; }
 		public string SerializedSelectedItem { get; set; }
 
