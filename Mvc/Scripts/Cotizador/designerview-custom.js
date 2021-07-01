e21ef9dbe2b1eaac8b0edde04d6ae8faab5d79ff
Tomás Vera <tomas.vera@virtualdreams.io>
2021-07-01 15:49:30 -0300

    Update

--- Mvc/Scripts/Cotizador/designerview-custom.js
+++ Mvc/Scripts/Cotizador/designerview-custom.js
@@ -1,37 +1,19 @@
 ﻿(function () {
     // declare dependency on the sfSelectors module
+    angular.module('designer').requires.push('sfFields');
     angular.module('designer').requires.push('sfSelectors');
     angular.module('designer').requires.push('ngSanitize');
 
     angular.module('designer').controller('CustomCtrl', ['$scope', 'propertyService', function ($scope, propertyService) {
         $scope.feedback.showLoadingIndicator = true;
+        $scope.montoEnganche = "";
 
-        // initialize the selector model
-        $scope.librariesSelector = {
-            selectedItemId: [],
-            selectedItem: []
-        };
-
-        // whenever librariesSelector.selectedItemId changes, bind the new value to the widget controller property 
         $scope.$watch(
-            'librariesSelector.selectedItemId',
-            function (newSelectedItemId, oldSelectedItemId) {
-                if (newSelectedItemId !== oldSelectedItemId) {
-                    if (newSelectedItemId) {
-                        $scope.properties.SerializedSelectedItemId.PropertyValue = newSelectedItemId;
-                    }
-                }
-            },
-            true
-        );
-
-        // whenever librariesSelector.selectedItem changes, bind the new value to the widget controller property
-        $scope.$watch(
-            'librariesSelector.selectedItem',
-            function (newSelectedItem, oldSelectedItem) {
-                if (newSelectedItem !== oldSelectedItem) {
-                    if (newSelectedItem) {
-                        $scope.properties.SerializedSelectedItem.PropertyValue = JSON.stringify(newSelectedItem);
+            'montoEnganche',
+            function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    if (newValue) {
+                        $scope.properties.MontoEnganche.PropertyValue = newValue;
                     }
                 }
             },
@@ -42,14 +24,7 @@
             .then(function (data) {
                 if (data) {
                     $scope.properties = propertyService.toAssociativeArray(data.Items);
-
-                    // copy the selected item id so that the selector can initialize with the selected items
-                    var selectedItemId = $scope.properties.SerializedSelectedItemId.PropertyValue || null;
-                    $scope.librariesSelector.selectedItemId = selectedItemId;
-
-                    var selectedItem = $scope.properties.SerializedSelectedItem.PropertyValue || null;
-                    $scope.librariesSelector.selectedItem = selectedItem;
-
+                    $scope.montoEnganche = $scope.properties.MontoEnganche.properties || "0";
                 }
             },
                 function (data) {
