f2fb17908d3264de700311a5e577c26d28be0eb2
Tomás Vera <tomas.vera@virtualdreams.io>
2021-07-01 18:54:41 -0300

    Update

--- Mvc/GlobalConfig.cs
+++ Mvc/GlobalConfig.cs
@@ -10,5 +10,7 @@ namespace SitefinityWebApp.Mvc
         public static string Origin = "https://tfs-sitefinity.virtualdreams.io/";
         public static string UrlToyotaFinancial = "https://toyotafinancial--salt001.lightning.force.com";
         public static string UrlToyotaClientes = "https://salt001-toyotaclientes.cs43.force.com";
+        public static string UrlComunidad = "https://mytfs.toyotacredito.com.mx/s/login/?ec=302&startURL=%2Fs%2F";
+        public static string UrlOfertas = "https://creditotoyota.com.mx/home-delivery";
     }
 }
