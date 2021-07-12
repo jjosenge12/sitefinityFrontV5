6357f9e6b6acba7c68c65259700769a1f1109de1
Tomás Vera <tomas.vera@virtualdreams.io>
2021-07-12 11:48:32 -0300

    Update API

--- Content/scripts/config.js
+++ Content/scripts/config.js
@@ -1,5 +1,6 @@
 ﻿window.config = {
-    urlbase: 'https://tfs-sitefinity.virtualdreams.io:444/api/tfsm',
+    //urlbase: 'https://tfs-sitefinity.virtualdreams.io:444/api/tfsm',
+    urlbase: 'https://localhost:44386/api/tfsm',
     urlToyotaFinancial: 'https://toyotafinancial--salt001.lightning.force.com',
     urlToyotaClientes: 'https://salt001-toyotaclientes.cs43.force.com',
     urlMyTfsm: 'https://mytfs.toyotacredito.com.mx/s/login/?ec=302&startURL=%2Fs%2F',
