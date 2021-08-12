2372e3145e839f86c6f05ed99a664a1523680241
Tomás Vera <tomas.vera@virtualdreams.io>
2021-08-12 15:54:26 -0300

    Add cambiar contraseña

---
+++ Content/scripts/Card/card-cambiar-contrasena.js
@@ -0,0 +1,33 @@
+﻿var token = "";
+
+$(document).ready(function () {
+    $.ajax(window.config.urlbase + "/GetAccessToken", {
+        beforeSend: showLoader,
+        complete: hideLoader,
+        success: function (data) {
+            token = data.result;
+        }
+    });
+
+    $("#cambiar-pass-form").submit(function (event) {
+        event.preventDefault();
+
+        var header = {
+            "Authorization": "Bearer " + token
+        };
+
+        var body = {
+            "rfc": $("#rfc").val(),
+            "password": $("#password").val()
+        };
+
+        $.ajax(window.config.UrlMySalesforce + '/services/apexrest/SetPasswordSitefinity', {
+            beforeSend: showLoader,
+            complete: hideLoader,
+            method: 'post',
+            success: function (res) {
+                console.log(res);
+            }
+        });
+    });
+});
