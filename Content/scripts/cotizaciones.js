baedc0ed6d5b0962422465ca3a5cec9e13c4945f
LucioRiusConde <lucio.conde@virtualdreams.io>
2022-02-25 16:42:58 -0300

    Update cotizaciones.js

--- Content/scripts/cotizaciones.js
+++ Content/scripts/cotizaciones.js
@@ -494,6 +494,17 @@ $(document).ready(() => {
         });
     }
 
+    function base64ToArrayBuffer(base64) {
+        var binaryString = window.atob(base64);
+        var binaryLen = binaryString.length;
+        var bytes = new Uint8Array(binaryLen);
+        for (var i = 0; i < binaryLen; i++) {
+            var ascii = binaryString.charCodeAt(i);
+            bytes[i] = ascii;
+        }
+        return bytes;
+    }
+
     //Elimina cotizaciones.
     $("#cotizacionModalDelete").click(function () {
         var _paramLeadId = sessionStorage.getItem("leadId");
