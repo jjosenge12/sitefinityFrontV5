10c049a8406d8f338066bf8e7d34b5c7688ed5ce
LucioRiusConde <lucio.conde@virtualdreams.io>
2022-04-04 11:09:59 -0300

    Update cotizaciones.js

--- Content/scripts/cotizaciones.js
+++ Content/scripts/cotizaciones.js
@@ -26,7 +26,8 @@ $(document).ready(() => {
         "Access-Control-Allow-Credentials": true,
     };
 
-   
+    FloatLabel.init();
+       
     const settings = {
         method: "POST",
         headers: _myHeader,
@@ -612,7 +613,16 @@ $(document).ready(() => {
     });
 
     //Para form de persona moral
-    
+    $("#step-5-contact-form").validate({
+        rules: {
+            distribuidores: {
+                selectRequired: true
+            },
+            phone: {
+                required: true
+            }
+        }
+    });
 
     $("#step-5-distribuidores").on("select2:open", function () {
         $("#step-5-distribuidores").siblings("[class='focus-border']").addClass("active");
@@ -715,16 +725,62 @@ function commitSalesforcePlan() {
     });
 }
 
-$("#step-5-contact-form").validate({
-    rules: {
-        distribuidores: {
-            selectRequired: true
-        },
-        phone: {
-            required: true
-        }
-    }
-});
+const FloatLabel = (() => {
+    // add active class
+    const handleFocus = (e) => {
+        const target = e.target;
+        target.parentNode.classList.add("active");
+        target.parentNode.classList.add("focus");
 
-$("input + label").css({ "position": "absolute", "left": "20px", "top": "1.15em", "transition": "transform 200ms ease", "pointer-events": "none" });
-$("input:focus + label,input + label.freeze").css({ "font-size": "0.7em", "font-weight": "400", "transform": "translateY(-3em)" });
+        var placeholder = target.getAttribute("data-placeholder");
+        if (placeholder) {
+            target.setAttribute("placeholder", placeholder);
+        }
+    };
+
+    // remove active class
+    const handleBlur = (e) => {
+        const target = e.target;
+        if (!target.value) {
+            target.parentNode.classList.remove("active");
+        }
+        target.parentNode.classList.remove("focus");
+        target.removeAttribute("placeholder");
+    };
+
+    // register events
+    const bindEvents = (floatField) => {
+        // const floatField = element.querySelector("input");
+        floatField.addEventListener("focus", handleFocus);
+        floatField.addEventListener("blur", handleBlur);
+    };
+
+    // get DOM elements
+    const init = () => {
+        const floatContainers = document.querySelectorAll(".float-container");
+
+        floatContainers.forEach((element) => {
+            let input = element.querySelector("input");
+            let select = element.querySelector("select");
+
+            if (input) {
+                bindEvents(input);
+
+                if (input.value) {
+                    element.classList.add("active");
+                }
+            }
+
+            if (select) {
+                if (select.value) {
+                    element.classList.add("active");
+                }
+                bindEvents(select);
+            }
+        });
+    };
+
+    return {
+        init: init,
+    };
+})();
