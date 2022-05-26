d6dd483560554d49d5e78cd64ef415c86a7b4dff
a-luizaga-vd <alexander.luizaga@virtualdreams.io>
2022-05-26 14:07:46 -0300

    Update config.js

--- Content/scripts/config.js
+++ Content/scripts/config.js
@@ -1,8 +1,11 @@
-﻿window.config = {
+﻿window.config = {        
+    origin: "https://tfs-sitefinity.virtualdreams.io",
     urlbase: 'https://tfs-sitefinity.virtualdreams.io:444/api/tfsm',
     //urlbase: 'https://localhost:44386/api/tfsm',
-    urlToyotaFinancial: 'https://toyotafinancial--salt001.lightning.force.com',
+    urlToyotaFinancial: 'https://toyotafinancial--qatfs.lightning.force.com',
+    urlToyotaCotizaciones: 'https://toyotafinancial--qatfs.my.salesforce.com',
     urlToyotaClientes: 'https://salt001-toyotaclientes.cs43.force.com',
+    UrlMySalesforce: 'https://toyotafinancial--salt001.my.salesforce.com',
     urlMyTfsm: 'https://mytfs.toyotacredito.com.mx/s/login/?ec=302&startURL=%2Fs%2F',
     reCaptchaSiteKey: '6LcjOG8bAAAAAN7E9hTFRkZ89DzWpfkESIYMh1Wo',
     reCaptchaSecretKey: '6LcjOG8bAAAAAOZvGzH7X_QtizU9Kzl5Iot79WYT'
